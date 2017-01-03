9991b57c0dd968159e22fae8004bc2f4d8397d70
dzdaniel84 <dzdaniel84@gmail.com>
2017-01-03 14:54:13 -0800

    fixed bug

--- python/props.py
+++ python/props.py
@@ -25,32 +25,36 @@ class Prop:
         return Not(self)
 
     def __eq__(self, that):
-        return all([self(truth) == that(truth) for truth in opts(max(self.ord, that.ord) + 1)])
+         o = max(self.ord, that.ord) + 1
+         for truth in opts(o):
+             if self(truth) != that(truth):
+                 return False
+         return True
 
     def table(self):
-        print('\n\033[1mPropositional Statement:\033[0m ' + str(self) + "\n")
-        def print_header(o, e):
-            print('╔══' + '══╦══'.join('═' for i in range(o)) + '══╦══' + '═'*len(str(e)) + "══╗")
-            print('║  ' + '  ║  '.join(char(i) for i in range(o)) + '  ║  ' + str(e)+ '  ║')
-            print('╠══' + '══╬══'.join('═' for i in range(o)) + '══╬══' + '═'*len(str(e)) + "══╣")
-
-        def print_row(truth, result):
-            print('║  ' + '  ║  '.join(single_letter(truth[i]) for i in range(len(truth))) \
-                + '  ║  ' + ' ' * (len(str(self))//2) + single_letter(result) + \
-                ' ' * (len(str(self))//2 - (1-len(str(self))%2)) + '  ║')
-
-        def print_footer(o, e):
-            print('╚══' + '══╩══'.join('═' for i in range(o)) + '══╩══' + '═'*len(str(e)) + "══╝")
-
-        def single_letter(b):
-            return '\033[92mT\033[0m' if b else '\033[91mF\033[0m'
-
         o = self.ord + 1
 
-        print_header(o, self)
+        def print_header():
+            print('\n\033[1mPropositional Statement:\033[0m ' + str(self) + "\n")
+            print('╔══' + '══╦══'.join('═' for i in range(o)) + '══╦══' + '═'*len(str(self)) + "══╗")
+            print('║  ' + '  ║  '.join(char(i) for i in range(o)) + '  ║  ' + str(self)+ '  ║')
+            print('╠══' + '══╬══'.join('═' for i in range(o)) + '══╬══' + '═'*len(str(self)) + "══╣")
+
+        def print_row(truth, result):
+            print('║  ' + '  ║  '.join(single_letter(truth[i]) for i in range(len(truth))) + \
+                '  ║  ' + ' ' * (len(str(self))//2) + single_letter(result) + \
+                ' ' * (len(str(self))//2 - (1-len(str(self))%2)) + '  ║')
+
+        def print_footer():
+            print('╚══' + '══╩══'.join('═' for i in range(o)) + '══╩══' + '═'*len(str(self)) + "══╝")
+
+        def single_letter(b):
+            return '\033[94mT\033[0m' if b else '\033[91mF\033[0m'
+
+        print_header()
         for truth in opts(o):
             print_row(truth, self(truth))
-        print_footer(o, self)
+        print_footer()
 
 
 class Const(Prop):
